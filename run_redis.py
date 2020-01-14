87088e59c82e2ff71051d0631772dd39378644d4
zzz686970 <1564256031@qq.com>
2020-01-14 22:36:03 +0800

    update redis file

--- run_redis.py
+++ run_redis.py
@@ -1,12 +1,32 @@
 import redis 
 from datetime import date 
+import struct 
+import zlib 
 
-r = redis.Redis()
+"""struct
+< little-endian 
+> big-endian
+= native
+! network
 
-r.set('foo', bar)
-value = r.get('foo')
+I unsigned int
+Q unsigned long long 
+"""
 
-print(value)
+for x in [[1,2,3], [1,2,3,4,5,6,7,8,9,10]]:
+	val = struct.pack("<%dI" % len(x), *x)
+	val_compress = zlib.compress(struct.pack("<%dI" % len(x), *x))
+	# val = struct.pack("I" * len(x) , *x)
+	print(val, len(val), val_compress, len(val_compress))
+	unpack_val = struct.unpack("<%dI" % (len(val) // 4), val)
+	unpack_val_decompress = struct.unpack("<%dI" % (len(zlib.decompress(val_compress)) // 4), zlib.decompress(val_compress))
+	print(unpack_val, unpack_val_decompress)
+# r = redis.Redis()
+
+# r.set('foo', bar)
+# value = r.get('foo')
+
+# print(value)
 
 def functions(mappings, key):
 	r.mset(mappings)
